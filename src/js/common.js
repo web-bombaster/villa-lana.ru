ff0b5e14468690ce4a82a1099dc131c0c28ee36f
Yurij Moshatin <y.moshatin@rbru-test.ru>
2024-04-26 17:59:23 +0300

    Начал делать хедер

--- src/js/common.js
+++ src/js/common.js
@@ -1,7 +1,7 @@
 
 document.addEventListener(
 	"DOMContentLoaded", () => {
-		// include './myscripts/preloader.js'
+		// -include './myscripts/preloader.js'
 		// include './myscripts/burger.js'
 		// include './myscripts/form.js'
 		// include './myscripts/accordeon.js'
